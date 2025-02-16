d0dea00211a95cfa4e812044c9b3c2476d4633f6
Victor Ose Iyayi <veektaw@gmail.com>
2025-02-16 11:14:37 +0100

    Added conditonl class name for keyboard

--- src/components/keyboard.jsx
+++ src/components/keyboard.jsx
@@ -1,14 +1,41 @@
+import { clsx } from "clsx";
 import React from "react";
 
 export default function Keyboard() {
   const alphabets = "abcdefghijklmnopqrstuvwxyz";
   const [currentAlphabets, setCurrentAlphabets] = React.useState(alphabets);
-  const letterElements = currentAlphabets
-    .split("")
-    .map((letter, index) => <span key={index}>{letter.toUpperCase()}</span>);
+  const [guessedLetters, setGuessedLetters] = React.useState([]);
+
+  function handleGuess(letter) {
+    setGuessedLetters((prevGuessedLetters) =>
+      prevGuessedLetters.includes(letter)
+        ? prevGuessedLetters
+        : [...prevGuessedLetters, letter]
+    );
+  }
+  const keyboardElements = currentAlphabets.split("").map((letter, index) => {
+    const isGuessed = guessedLetters.includes(letter);
+    const isCorrect = isGuessed && currentWord.includes(letter);
+    const isWrong = isGuessed && !currentWord.includes(letter);
+    const className = clsx({
+      correct: isCorrect,
+      wrong: isWrong,
+    });
+    return (
+      <button
+        className={className}
+        onClick={() => handleGuess(letter)}
+        key={letter}
+      >
+        {letter.toUpperCase()}
+      </button>
+    );
+  });
+  console.log(guessedLetters);
   return (
     <>
-      <section>{letterElements}</section>
+      <section className="keyboard">{keyboardElements}</section>
+      <button className="new-game">New game</button>
     </>
   );
 }
